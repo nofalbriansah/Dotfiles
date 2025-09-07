b8a4bc3eff78b81cdc7d326ca90ea8861480447c
nofalbriansah <nofalbriansah@gmail.com>
2025-09-07 15:47:39 +0700

    refactor:  move ABDownloadManager to flake overlay

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -7,53 +7,116 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, unstable, home-manager, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, unstable, home-manager, ... }: let
+    system = "x86_64-linux";
 
+    # Overlay for custom packages
+    # ABDownloadManager as a custom package
+    overlays = [
+      (final: prev: {
+        abdownloadmanager = prev.stdenv.mkDerivation rec {
+          pname = "abdownloadmanager";
+          version = "1.6.10";
+
+          # Source tarball from GitHub releases
+          src = prev.fetchurl {
+            url = "https://github.com/amir1376/ab-download-manager/releases/download/v${version}/ABDownloadManager_${version}_linux_x64.tar.gz";
+            sha256 = "D0ZwRwdXr+Y+xnC2ZWjSTSHeUAkmgM3mOCRWcZOc7Is=";
+          };
+
+          nativeBuildInputs = [ prev.makeWrapper ];
+
+          buildInputs = with prev; [
+            xorg.libXtst
+            xorg.libXext
+            xorg.libXrender
+            xorg.libXi
+            glib
+            gtk3
+            libappindicator-gtk3
+          ];
+
+          unpackPhase = ''
+            tar -xzf $src
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/opt/abdownloadmanager
+            cp -r ABDownloadManager/* $out/opt/abdownloadmanager
+
+            chmod +x $out/opt/abdownloadmanager/bin/ABDownloadManager
+
+            mkdir -p $out/bin
+            makeWrapper $out/opt/abdownloadmanager/bin/ABDownloadManager \
+              $out/bin/abdm \
+              --set LD_LIBRARY_PATH "${prev.lib.makeLibraryPath buildInputs}"
+
+            runHook postInstall
+          '';
+        };
+      })
+    ];
+  in {
+    # NixOS system configuration
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      # Special arguments for unstable packages
       specialArgs = {
         unstable = import unstable {
-          system = "x86_64-linux";
+          inherit system;
           config.allowUnfree = true;
         };
       };
 
+      # NixOS modules
       modules = [
         ./nixos/configuration.nix
-        
+
         {
+          # Apply overlays (custom packages)
+          nixpkgs.overlays = overlays;
+
+          # Proxy for GUI apps and system networking
           networking.proxy = {
-            httpProxy = "http://127.0.0.1:7897";
-            httpsProxy = "http://127.0.0.1:7897";
-            noProxy = "127.0.0.1,localhost,::1";
+            httpProxy  = "http://127.0.0.1:7890";
+            httpsProxy = "http://127.0.0.1:7890";
+            noProxy    = "127.0.0.1,localhost,::1";
           };
 
+          # Environment variables for shell and user applications
           environment.sessionVariables = {
-            http_proxy = "http://127.0.0.1:7897";
-            https_proxy = "http://127.0.0.1:7897";
-            no_proxy = "127.0.0.1,localhost,::1";
+            http_proxy  = "http://127.0.0.1:7890";
+            https_proxy = "http://127.0.0.1:7890";
+            no_proxy    = "127.0.0.1,localhost,::1";
           };
         }
 
+        # Home Manager integration
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.backupFileExtension = "backup";
 
+          # Special args for unstable packages
           home-manager.extraSpecialArgs = {
             unstable = import unstable {
-              system = "x86_64-linux";
+              inherit system;
               config.allowUnfree = true;
             };
           };
 
+          # Import user home configuration
           home-manager.users.nbs = import ./home/home.nix;
         }
       ];
     };
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+
+    # Formatter for flake (nixpkgs-fmt)
+    formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
   };
 }
-
 
--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -1,53 +1,8 @@
 { config, pkgs, unstable, ... }:
 
-let
-  abdownloadmanager = pkgs.stdenv.mkDerivation rec {
-    pname = "abdownloadmanager";
-    version = "1.6.10";
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/amir1376/ab-download-manager/releases/download/v${version}/ABDownloadManager_${version}_linux_x64.tar.gz";
-      sha256 = "D0ZwRwdXr+Y+xnC2ZWjSTSHeUAkmgM3mOCRWcZOc7Is=";
-    };
-    
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-    
-    buildInputs = with pkgs; [
-      xorg.libXtst
-      xorg.libXext
-      xorg.libXrender
-      xorg.libXi
-      glib
-      gtk3
-      libappindicator-gtk3
-    ];
-    
-    unpackPhase = ''
-      tar -xzf $src
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/opt/abdownloadmanager
-      cp -r ABDownloadManager/* $out/opt/abdownloadmanager
-
-      chmod +x $out/opt/abdownloadmanager/bin/ABDownloadManager
-
-      mkdir -p $out/bin
-      makeWrapper $out/opt/abdownloadmanager/bin/ABDownloadManager \
-        $out/bin/abdm \
-        --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath buildInputs}"
-
-      runHook postInstall
-    '';
-  };
-in
-
 {
   imports = [
     ./gnome.nix
-    #./hyprland.nix
   ];
 
   home.username = "nbs";
@@ -55,93 +10,108 @@ in
   home.stateVersion = "25.05";
 
   ######################
-  # Env vars
+  # Environment Variables
   ######################
   home.sessionVariables = {
-    ANDROID_HOME = "~/Developments/android/Sdk";
-    ANDROID_SDK_ROOT = "~/Developments/android/Sdk";
-    JAVA_HOME = "${pkgs.jdk21}";
-    CHROME_EXECUTABLE = "${unstable.google-chrome}/bin/google-chrome-stable";
-    TERMINAL = "ghostty";
+    ANDROID_HOME       = "~/Developments/android/Sdk";
+    ANDROID_SDK_ROOT   = "~/Developments/android/Sdk";
+    JAVA_HOME          = "${pkgs.jdk21}";
+    CHROME_EXECUTABLE  = "${unstable.google-chrome}/bin/google-chrome-stable";
+    TERMINAL           = "ghostty";
   };
 
   ######################
   # Packages
   ######################
   home.packages = with pkgs; [
-    # CLI tools
-    curl
-    git
+
+    ######################
+    # CLI Tools
+    ######################
+    neofetch
     stow
     tree
-    wget
-    zip
     unzip
     unrar
-    neofetch
-    python3
-    clang
-    cmake
     vim
-    ninja
-    pkg-config
+    wget
+    zip
     nixpkgs-fmt
-    android-tools
-    scrcpy
-    jdk
-    mesa-demos
-    go
-    bun
-    unstable.hugo
+    
+    ######################
+    # Themes
+    ######################
+    colloid-icon-theme
+    marble-shell-theme
+    reversal-icon-theme
 
-    # GUI apps
-    unstable.firefox
-    unstable.google-chrome
-    unstable.obsidian
-    unstable.flclash
-    thunderbird
-    onlyoffice-desktopeditors
-    gnome-pomodoro
-    obs-studio
-    gimp3-with-plugins
-    zotero
+    ######################
+    # GUI Apps (stable)
+    ######################
+    abdownloadmanager
     discord
-    vlc
+    gimp3-with-plugins
+    ghostty
+    gnome-disk-utility
+    gnome-pomodoro
+    gnome-system-monitor
     heroic-unwrapped
     kdePackages.kdenlive
-    ghostty
     localsend
+    obs-studio
+    onlyoffice-desktopeditors
+    thunderbird
+    vlc
     zoom-us
     slack
-    abdownloadmanager
-    gnome-system-monitor
-    gnome-disk-utility
+    zotero
 
-    # Themes
-    colloid-icon-theme
-    reversal-icon-theme
-    marble-shell-theme
+    ######################
+    # GUI Apps (unstable)
+    ######################
+    unstable.firefox
+    unstable.flclash
+    unstable.google-chrome
+    unstable.obsidian
 
-    # SDK / IDE
-    unstable.gemini-cli
-    unstable.flutter
+    ######################
+    # Dev / SDK / IDE Tools
+    ######################
+    android-tools
+    bun
+    clang
+    cmake
+    curl
+    git
+    go
+    jdk
+    ninja
+    mesa-demos
+    pkg-config
+    python3
+    scrcpy
+    
     unstable.android-studio
-    unstable.vscode
     unstable.code-cursor-fhs
-    unstable.zed-editor
+    unstable.flutter
+    unstable.gemini-cli
     unstable.godot
+    unstable.hugo
     unstable.jetbrains.goland
+    unstable.vscode
+    unstable.zed-editor
   ];
 
   ######################
-  # Programs
+  # Programs Configuration
   ######################
   programs = {
+
     starship = {
       enable = true;
       settings = {
         add_newline = true;
-        format = "$all";
+        format      = "$all";
       };
     };
 
@@ -153,21 +123,25 @@ in
       '';
       shellAbbrs = {
         dot = "cd ~/Dotfiles";
-        
-        gs = "git status";
-        ga = "git add .";
-        gf = "git fetch";
-        gp = "git push";
+
+        # Git shortcuts
+        ga  = "git add .";
+        gf  = "git fetch";
+        gs  = "git status";
+        gp  = "git push";
+        gd  = ''git commit -m "docs: "'';
         gft = ''git commit -m "feat: "'';
         gfx = ''git commit -m "fix: "'';
-        gc = ''git commit -m "chore: "'';
+        gc  = ''git commit -m "chore: "'';
         gr  = ''git commit -m "refactor: "'';
-        gd  = ''git commit -m "docs: "'';
 
+        # NixOS shortcuts
         ng = "sudo nixos-rebuild list-generations";
         ns = "sudo nixos-rebuild switch --flake ~/Dotfiles/nixos";
         nr = "sudo nixos-rebuild switch --rollback";
         nd = "sudo nix-collect-garbage -d";
+
+        # Edit config shortcuts
         nc = "nano ~/Dotfiles/nixos/nixos/configuration.nix";
         nf = "nano ~/Dotfiles/nixos/flake.nix";
         nh = "nano ~/Dotfiles/nixos/home/home.nix";
@@ -175,3 +149,4 @@ in
     };
   };
 }
+
